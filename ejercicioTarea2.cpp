9a0e4de1f8579749e2a26843e3e388cfc87dca30
Arauz <86898345+kjarj54@users.noreply.github.com>
2023-03-28 21:28:08 -0600

    a

--- ejercicioTarea2.cpp
+++ ejercicioTarea2.cpp
@@ -6,21 +6,18 @@ using namespace std;
 int main(){
     int c; 
     int palabras = 0;
-    int separador = 1; 
+    int anterior = ' '; 
 
     cout<<("Ingrese el texto que desea contar las palabras:\n");
     
-    do {
-        c = getchar(); // Lee un car�cter 
-        if (isspace(c) || c == EOF) { // Si el car�cter es un espacio o el fin de archivo
-            if (!separador) { // Si no hab�a un separador antes
-                palabras++; // Incrementa el contador de palabras
-                separador = 1; // Indica que hay un separador
-            }
-        }else { // Si el car�cter no es un espacio ni el fin de archivo
-            separador = 0; // Indica que no hay un separador
-        }
-    } while (c != EOF); // Repite hasta que se encuentre el fin de archivo
+    c = getchar(); // leer un caracter desde el teclado
+	while (c != EOF) { // repetir hasta que se encuentre el fin de la entrada
+    	if (isspace(c) && !isspace(anterior)) { // si el caracter es un espacio y el anterior no lo era
+      		palabras++; 
+    	}
+    	anterior = c; // actualizar el caracter anterior
+   		c = getchar(); // leer otro caracter desde el teclado
+  	}
 
     cout<<("La cantidad de palabras es:\n");
     cout<<(palabras); // Muestra el resultado
